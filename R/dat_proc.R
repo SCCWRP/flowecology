0c6da8072f3ecf1ecaf93fdd3e89de5ca5d45856
fawda123 <mbafs2012@gmail.com>
2018-07-01 13:17:56 -0700

    working on biology maps, plots

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -2,8 +2,25 @@
 
 library(tidyverse)
 library(sf)
+library(lubridate)
 
 # biological data linked to nhd
-
-biodat <- st_read('Z:/JennyTaylor/Data Visualization/Species_NHD.shp')
+biodat <- st_read('//172.16.1.5/Data/JennyTaylor/Data Visualization/Species_NHD.shp')
+biodat <- biodat %>% 
+  dplyr::select(FID_1, date, name, occurrence, source) %>% 
+  mutate(
+    yr = year(date), 
+    yr = ifelse(yr == 2028, 2018, yr),
+    mo = month(date),
+    occurrence = factor(occurrence, levels = c(0, 1), labels = c('absent', 'present')),
+    occol = ifelse(occurrence == 'present', 'lightgreen', 'tomato1'),
+    occex = ifelse(occurrence == 'present', 7, 3)
+  )
 save(biodat, file = 'data/biodat.RData', compress = 'xz')
+
+# reaches in LA/Ventura counties, RB4
+rchdat <- st_read('//172.16.1.5/Biology/Flow ecology and climate change_ES/Data/RawData/Environmental Data/Rivers/RB4_Rivers.shp') %>% 
+  st_simplify(dTolerance = 30, preserveTopology = T)
+
+save(rchdat, file = 'data/rchdat.RData', compress = 'xz')
+
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # README
 
 TAG response email summary [here](https://sccwrp.github.io/flowecology/doc/emails.html)
+
+Website [here](http://162.243.131.102:3838/flowecology/)
+
+## Data
+
+* `biodat.RData` compiled biogy data, species p/a by date and location
+
+* `rchdat.RData` NHD flow reaches for RB4 region
