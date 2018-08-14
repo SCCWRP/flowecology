b92b079a79fecea58c2d3990c756087c7d51c4f9
fawda123 <mbafs2012@gmail.com>
2018-08-14 13:03:43 -0700

    adding some flow data, trying to mapedit select features

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -4,6 +4,10 @@ library(tidyverse)
 library(sf)
 library(lubridate)
 
+# epsg code
+prj <- 26911
+
+######
 # biological data linked to nhd
 biodat <- st_read('//172.16.1.5/Data/JennyTaylor/Data Visualization/Species_NHD.shp')
 biodat <- biodat %>% 
@@ -19,9 +23,45 @@ biodat <- biodat %>%
   )
 save(biodat, file = 'data/biodat.RData', compress = 'xz')
 
+######
 # reaches in LA/Ventura counties, RB4
 rchdat <- st_read('//172.16.1.5/Biology/Flow ecology and climate change_ES/Data/RawData/Environmental Data/Rivers/RB4_Rivers.shp') %>% 
-  st_simplify(dTolerance = 30, preserveTopology = T)
+  st_simplify(dTolerance = 30, preserveTopology = T) %>% 
+  st_transform(prj)
 
 save(rchdat, file = 'data/rchdat.RData', compress = 'xz')
 
+######
+# pour points and watersheds with modelled historic flow
+
+# reaches in LA/Ventura counties, RB4
+prsdat <- st_read('ignore/PP_20180405and0414.shp') %>% 
+  st_transform(prj) %>% 
+  rename(unq_id = unq_ID) %>% 
+  mutate(unq_id = as.character(unq_id))
+
+save(prsdat, file = 'data/prsdat.RData', compress = 'xz')
+
+# catchment data
+shddat1 <- st_read('ignore/Catchments_Merge_20180405.shp') %>% 
+  st_transform(prj) %>% 
+  select(unq_ID) %>% 
+  rename(unq_id = unq_ID)
+shddat2 <- st_read('ignore/ws_20180414.shp') %>% 
+  st_transform(prj) 
+shddat <- rbind(shddat1, shddat2)
+
+save(shddat, file = 'data/shddat.RData', compress = 'xz')
+
+#######
+# modelled historic flow data for each pour point in prsdat
+
+flodat <- read.csv('ignore/From Kelly_MERGE_DailyAverages_ws001to048.csv') %>% 
+  rename(date = X) %>% 
+  mutate(
+    date = as.character(date),
+    date = as.Date(date, format = '%m/%d/%Y %H:%M')
+    ) %>% 
+  gather('unq_id', 'flo', -date) 
+
+save(flodat, file = 'data/flodat.RData', compress = 'xz')
--- README.md
+++ README.md
@@ -8,4 +8,10 @@ Website [here](http://162.243.131.102:3838/flowecology/)
 
 * `biodat.RData` compiled biogy data, species p/a by date and location
 
+* `flodat.RData` modelled historic daily flow data at pour points in `prsdat.RData`
+
+* `prsdat.RData` sf object of watershed pour points with modelled historic flow data
+
 * `rchdat.RData` NHD flow reaches for RB4 region
+
+* `shddat.RData` sf object of watershed polygons with modelled historic flow data, can be matched to `prsdat.RData`
