ba89db3bde78f9bc8f577e15f84e83bde4c95741
fawda123 <mbafs2012@gmail.com>
2019-06-30 11:30:02 -0700

    basic biology page added

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -3,65 +3,83 @@
 library(tidyverse)
 library(sf)
 library(lubridate)
+library(proj4shortcut)
 
 # epsg code
 prj <- 26911
+prjutm <- utm_wgs84(11)
 
-######
+# biology data ------------------------------------------------------------
+
 # biological data linked to nhd
-biodat <- st_read('//172.16.1.5/Data/JennyTaylor/Data Visualization/Species_NHD.shp')
+biodat <- st_read('//172.16.1.5/Biology/Flow ecology and climate change_ES/Marcus/data_and_scripts/species_occurrence_unique_NHDFlowline_JOIN_NAD83.shp')
 biodat <- biodat %>% 
-  dplyr::select(FID_1, date, name, occurrence, source) %>% 
+  dplyr::select(FID_1, date, name, occurrence, COMID) %>% 
   mutate(
+    date = as.character(date),
     date = ifelse(date == as.Date('2028-07-07'), as.Date('2018-07-07'), date),
     date = as.Date(date, origin = "1970-01-01"),
+    name = as.character(name), 
     yr = year(date), 
     mo = month(date),
     occurrence = factor(occurrence, levels = c(0, 1), labels = c('absent', 'present')),
     occol = ifelse(occurrence == 'present', 'lightgreen', 'tomato1'),
     occex = ifelse(occurrence == 'present', 7, 3)
-  )
+  ) %>% 
+  filter(name %in% c('arroyo chub', 'arroyo toad', 'least bell\'s vireo', 'rainbow trout', 'santa ana sucker', 'southwestern pond turtle')) %>% 
+  st_transform(prj)
+
 save(biodat, file = 'data/biodat.RData', compress = 'xz')
 
-######
-# reaches in LA/Ventura counties, RB4
-rchdat <- st_read('//172.16.1.5/Biology/Flow ecology and climate change_ES/Data/RawData/Environmental Data/Rivers/RB4_Rivers.shp') %>% 
-  st_simplify(dTolerance = 30, preserveTopology = T) %>% 
+# Reach shapefile ---------------------------------------------------------
+
+# ##
+# # evalue file size and plot times for difference tolerance values
+# toeval <- tibble(
+#   tols = seq(0, 1000, by = 10),
+#   size = NA, 
+#   plttm = NA
+# )
+# 
+# rchdat <- st_read('//172.16.1.5/Biology/Flow ecology and climate change_ES/Marcus/data_and_scripts/Flowline.shp') %>% 
+#   st_transform(prjutm) 
+# 
+# for(i in 1:nrow(toeval)){
+#   
+#   cat(i, '\t')
+#   
+#   tolvl <- toeval[i, ] %>% pull(tols)
+#   
+#   tosv <- rchdat %>%   
+#     st_simplify(dTolerance = tolvl, preserveTopology = T) %>%
+#     st_transform(prj)
+#   
+#   save(tosv, file = 'data/tosv.RData', compress = 'xz')
+#   
+#   flsz <- file.info('data/tosv.RData')$size / 1000
+#   
+#   strt <- Sys.time()
+#   mapview(tosv)
+#   elaps <- Sys.time() - strt
+#   
+#   toeval[i, 'size'] <- flsz
+#   toeval[i, 'plttm'] <- as.numeric(elaps)
+#   
+# }
+# 
+# file.remove('data/tosv.RData')
+# 
+# plot(size ~ tols, toeval, type = 'l')
+# plot(plttm ~ tols, toeval, type = 'l')
+
+rchdat <- st_read('//172.16.1.5/Biology/Flow ecology and climate change_ES/Marcus/data_and_scripts/Flowline.shp') %>% 
+  st_transform(prjutm) %>% 
+  st_simplify(dTolerance = 220, preserveTopology = T) %>%
   st_transform(prj)
 
 save(rchdat, file = 'data/rchdat.RData', compress = 'xz')
 
-######
-# pour points and watersheds with modelled historic flow
+# Metrics -----------------------------------------------------------------
 
-# reaches in LA/Ventura counties, RB4
-prsdat <- st_read('ignore/PP_20180405and0414.shp') %>% 
-  st_transform(prj) %>% 
-  rename(unq_id = unq_ID) %>% 
-  mutate(unq_id = as.character(unq_id))
-
-save(prsdat, file = 'data/prsdat.RData', compress = 'xz')
-
-# catchment data
-shddat1 <- st_read('ignore/Catchments_Merge_20180405.shp') %>% 
-  st_transform(prj) %>% 
-  select(unq_ID) %>% 
-  rename(unq_id = unq_ID)
-shddat2 <- st_read('ignore/ws_20180414.shp') %>% 
-  st_transform(prj) 
-shddat <- rbind(shddat1, shddat2)
-
-save(shddat, file = 'data/shddat.RData', compress = 'xz')
-
-#######
-# modelled historic flow data for each pour point in prsdat
-
-flodat <- read.csv('ignore/From Kelly_MERGE_DailyAverages_ws001to048.csv') %>% 
-  rename(date = X) %>% 
-  mutate(
-    date = as.character(date),
-    date = as.Date(date, format = '%m/%d/%Y %H:%M')
-    ) %>% 
-  gather('unq_id', 'flo', -date) 
-
-save(flodat, file = 'data/flodat.RData', compress = 'xz')
+# flow metrics calculated where biology was observed
+file.copy('../flowmetrics/data/bioflowmetest.RData', 'data/')
--- README.md
+++ README.md
@@ -6,10 +6,4 @@ Website [here](https://sccwrp.shinyapps.io/flowecology/)
 
 * `biodat.RData` compiled biogy data, species p/a by date and location
 
-* `flodat.RData` modelled historic daily flow data at pour points in `prsdat.RData`
-
-* `prsdat.RData` sf object of watershed pour points with modelled historic flow data
-
 * `rchdat.RData` NHD flow reaches for RB4 region
-
-* `shddat.RData` sf object of watershed polygons with modelled historic flow data, can be matched to `prsdat.RData`
